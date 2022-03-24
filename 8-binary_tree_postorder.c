1112685808487683289ce7974737be7561f3dcdf
j4ir0st <jairocast2@gmail.com>
2022-03-24 15:01:15 -0500

    goes through a binary tree using post-order traversal with betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through binary tree using post-order traversal
  * @tree: tree to traverse
  * @func: function to print
  */

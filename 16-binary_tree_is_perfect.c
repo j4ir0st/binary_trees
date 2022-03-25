6bb3487a1661705473a86f4e9a17ea79dfae0d2c
j4ir0st <jairocast2@gmail.com>
2022-03-24 20:19:40 -0500

    checks if a binary tree is perfect with betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
-        left = binary_tree_height(tree->left);
-        right = binary_tree_height(tree->right);
-        return (MAX(left, right) + 1);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (MAX(left, right) + 1);
 }
 
 /**
@@ -45,15 +45,15 @@ int pow_recursion(int x, int y)
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left == NULL && tree->right == NULL)
-                return (1);
-        left = binary_tree_leaves(tree->left);
-        right = binary_tree_leaves(tree->right);
-        return (left + right);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	return (left + right);
 }
 
 /**
